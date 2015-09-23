ea34462ee1efa0da1118894644ff52702fc50e08
Dan VerWeire <dverweire@gmail.com>
2015-09-23 16:25:26 -0400

    Split mem store into own file; rename master mem; add object-hash

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = require('./lib/cache');
 module.exports.MemStore = require('./lib/store-mem');
+module.exports.MaterMemStore = require('./lib/store-master-mem');
 module.exports.RedisStore = require('./lib/store-redis');
 module.exports.MemcachedStore = require('./lib/store-memcached');
 module.exports.MultiStore = require('./lib/store-multistore');
